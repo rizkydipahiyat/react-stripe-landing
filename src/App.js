d4f6c3e28770ec42b9cf79f2f1f33ca2dc220ca8
rizkydipahiyat <60695993+rizkydipahiyat@users.noreply.github.com>
2021-07-17 14:08:03 +0700

    Sidebar Components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Submenu from "./components/Submenu/Submenu";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
 	return (
 		<>
 			<Navbar />
-			{/* Sidebar */}
+			<Sidebar />
 			<Hero />
 			<Submenu />
 		</>
