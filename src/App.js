0f39eeb044456c7945270f098d4822489807b770
rizkydipahiyat <60695993+rizkydipahiyat@users.noreply.github.com>
2021-07-17 13:33:23 +0700

    Hero Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			{/* Sidebar */}
-			{/* Hero */}
+			<Hero />
 			{/* SubMenu */}
 		</>
 	);
